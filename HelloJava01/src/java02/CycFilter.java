cfd66445557be8465ea5c40f246a485680a01845
YWZFrances <1595642024@qq.com>
2018-01-02 17:24:32 +0800

    1212

--- HelloJava01/src/java02/CycFilter.java
+++ HelloJava01/src/java02/CycFilter.java
@@ -3,6 +3,16 @@ package java02;
 public class CycFilter {
 	public static void main(String[] args){
 		String[] array = new String[]{"老鹰","小鸟","老鹰","小鸟","老鹰","小鸟","老鹰","小鸟","老鹰","小鸟","老鹰","小鸟"};
-		
+		System.out.println("我的花园里有许多鸟，但是来了很多老鹰，请帮我抓走");
+		int laoying = 0;
+		for(String string:array){
+			if(string.equals("老鹰")){
+				System.out.println("发现一只老鹰，抓起来");
+				laoying++;
+				continue;
+			}
+			System.out.println("搜索鸟类，发现了"+string);
+		}
+		System.out.println("一共抓到了"+laoying+"只老鹰");
 	}
 }
